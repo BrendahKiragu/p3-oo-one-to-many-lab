bd6f9df8bea5fc7bbfaa63bee9e7024ba6113b7a
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-04 15:34:55 +0300

    one to many classes relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,26 @@
-class Pet:
-    pass
+class Owner: 
+    def __init__(self, name):
+        self.name = name
 
-class Owner:
-    pass
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self] 
+
+    def add_pet(self, pet):
+        if not isinstance (pet, Pet):
+            raise Exception("invalid input pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key = lambda pet:pet.name) 
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner = None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("pet_type input invalid!")
+        self.pet_type = pet_type
+        self.name = name
+        self.owner = owner
+        Pet.all.append(self)
+        
